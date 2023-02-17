39f5ba45e0e0cbcd9ebf080d90066e23241a3629
JP Belval <jpbelv@gmail.com>
2023-02-17 11:22:55 -0500

    Adaptation du BVH part 2

---
+++ JournalDeBord/Semaine4.md
@@ -0,0 +1,28 @@
+# Semaine 3
+
+### Description du travail
+
+#### 4.25h
+
+Découverte d'un problème avec mon implémentation du BVH (Bounding Volume Hierarchy), et recherche sur le sujet et l'implémentation en Rust pour obtenir une référence.
+
+#### 3.25 h
+
+Adaptation du BVH et tentative de résoudre les problèmes liés à l'ordering des nodes qui constituent des Hittables.
+
+#### 3.25 h
+
+Suite des changements aux BVH avec l'ajout de la fonction pour l'axis range.
+
+<hr/>
+#### ~15h
+
+### Connaissances acquises
+
+### Références
+
+https://doc.rust-lang.org/stable/rust-by-example/ Rust by Example pour apprendre les fonctionnalités du langage.
+
+https://github.com/andystanton/raytracer-rs/blob/master/src/raytracer.rs Utilisation du multithreading en Rust
+
+https://en.wikipedia.org/wiki/Bounding_volume_hierarchy Bounding volume hierarchy
--- Raytracing/src/bvh.rs
+++ Raytracing/src/bvh.rs
@@ -4,53 +4,60 @@ use crate::ray::Ray;
 use rand::Rng;
 use std::cmp::Ordering;
 
+//Le bvh fonctionne d'une manière semblable à un arbre binaire.
 enum BVHNode {
     Branch { left: Box<BVH>, right: Box<BVH> },
-    Leaf(Box<dyn Hittable>)
+    Leaf(Box<dyn Hittable>),
 }
 
 pub struct BVH {
     tree: BVHNode,
-    bbox: AABB
+    bbox: AABB,
 }
 
 impl BVH {
-    pub fn new(mut hittable:Vec<Box<dyn Hittable>>, time0: f32, time1: f32) -> Self {
+    pub fn new(mut hittable: Vec<Box<dyn Hittable>>, time0: f32, time1: f32) -> Self {
         fn box_compare(
             time0: f32,
             time1: f32,
             axis: usize,
         ) -> impl FnMut(&Box<dyn Hittable>, &Box<dyn Hittable>) -> Ordering {
             move |a, b| {
+                //Comparaison des bounding boxes
                 let a_bbox = a.bounding_box(time0, time1);
                 let b_bbox = b.bounding_box(time0, time1);
                 if a_bbox.is_none() || b_bbox.is_none() {
                     panic!("No bounding box")
-                }else{
+                } else {
                     if a_bbox.unwrap().min[axis] - b_bbox.unwrap().min[axis] < 0.0 {
                         Ordering::Less
                     } else {
                         Ordering::Greater
                     }
-
                 }
             }
         }
 
-        fn axis_range(hittable: &Vec<Box<dyn Hittable>>, time0: f32, time1: f32, axis: usize) -> f32 {
-            let (min, max) = hittable.iter().fold((f32::MAX, f32::MIN), |(bmin, bmax),hit| {
-                if let Some(aabb) = hit.bounding_box(time0, time1) {
-                    (bmin.min(aabb.min[axis]), bmax.max(aabb.max[axis]))
-                } else{
-                    (bmin, bmax)
-                }
-            }
-        );
-        max - min
+        fn axis_range(
+            hittable: &Vec<Box<dyn Hittable>>,
+            time0: f32,
+            time1: f32,
+            axis: usize,
+        ) -> f32 {
+            let (min, max) = hittable
+                .iter()
+                .fold((f32::MAX, f32::MIN), |(bmin, bmax), hit| {
+                    if let Some(aabb) = hit.bounding_box(time0, time1) {
+                        (bmin.min(aabb.min[axis]), bmax.max(aabb.max[axis]))
+                    } else {
+                        (bmin, bmax)
+                    }
+                });
+            max - min
         }
 
         let mut axis_ranges: Vec<(usize, f32)> = (0..3)
-            .map(|a| (a, axis_range(&hittable, time0, time1, axis)))
+            .map(|a| (a, axis_range(&hittable, time0, time1, a)))
             .collect();
 
         axis_ranges.sort_unstable_by(|a, b| b.1.partial_cmp(&a.1).unwrap());
@@ -64,38 +71,47 @@ impl BVH {
             1 => {
                 let leaf = hittable.pop().unwrap();
                 if let Some(bbox) = leaf.bounding_box(time0, time1) {
-                    BVH { tree: BVHNode::Leaf(leaf), bbox}
-                }else {
+                    BVH {
+                        tree: BVHNode::Leaf(leaf),
+                        bbox,
+                    }
+                } else {
                     panic!["no bounding box"]
                 }
-            },
+            }
             _ => {
                 let right = BVH::new(hittable.drain(len / 2..).collect(), time0, time1);
                 let left = BVH::new(hittable, time0, time1);
-                let bbox = surrounding_box(&left.bb, &right.bbox);
-                BVH { tree: BVHNode::Branch { left: Box::new(left), right: Box::new(right) }, bbox}
+                let bbox = surrounding_box(&left.bbox, &right.bbox);
+                BVH {
+                    tree: BVHNode::Branch {
+                        left: Box::new(left),
+                        right: Box::new(right),
+                    },
+                    bbox,
+                }
             }
         }
     }
-
 }
 
 impl Hittable for BVH {
-    fn hit(&self, ray: &Ray, t_min: f32, t_max: f32) -> Option<HitRecord> {
+    fn hit(&self, ray: &Ray, t_min: f32, mut t_max: f32) -> Option<HitRecord> {
         if self.bbox.hit(&ray, t_min, t_max) {
-            let left = self.left.hit(&ray, t_min, t_max);
-            let right = self.right.hit(&ray, t_min, t_max);
-            match (left, right) {
-                (Some(l), Some(r)) => {
-                    if l.t < r.t {
-                        Some(l)
+            match &self.tree {
+                BVHNode::Leaf(leaf) => leaf.hit(&ray, t_min, t_max),
+                BVHNode::Branch { left, right } => {
+                    let left = left.hit(&ray, t_min, t_max);
+                    if let Some(l) = &left {
+                        t_max = l.t
+                    };
+                    let right = right.hit(&ray, t_min, t_max);
+                    if right.is_some() {
+                        right
                     } else {
-                        Some(r)
+                        left
                     }
                 }
-                (Some(l), None) => Some(l),
-                (None, Some(r)) => Some(r),
-                _ => None,
             }
         } else {
             None
@@ -103,6 +119,6 @@ impl Hittable for BVH {
     }
 
     fn bounding_box(&self, t0: f32, t1: f32) -> Option<AABB> {
-        Some(self.bbox.clone())
+        Some(self.bbox)
     }
 }
